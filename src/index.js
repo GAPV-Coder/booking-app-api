374eaf91a2ee1314fe12c7cc051b64f28393f26a
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2022-12-12 18:15:52 -0500

    Add user authentication

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import hotelsRoute from "./routes/hotels.routes.js";
+import authRoute from "./routes/auth.routes.js";
 
 const app = express();
 dotenv.config();
 
 import _connect from "./database/db.js";
 
-// Middleware
+// Middlewares
+app.use(cookieParser());
 app.use(express.json());
 
+app.use("/api/auth", authRoute);
 app.use("/api/hotels", hotelsRoute);
+
+app.use((err, req, res, next) => {
+	const errorStatus = err.status || 500;
+	const errorMessage = err.message || "Something went wrong!";
+	return res.status(errorStatus).json({
+		success: false,
+		status: errorStatus,
+		message: errorMessage,
+		stack: err.stack
+	});
+});
 
 app.listen(process.env.PORT || 8800, () => {
 	_connect();
